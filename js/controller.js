9de9a553f81e71a10b115ec5ee59f817c85a3752
slavco86 <slavick.slavick@gmail.com>
2017-09-19 00:56:10 +0100

    updated gitignore, cleaned up controller JS and main CSS for release

--- js/controller.js
+++ js/controller.js
@@ -2,14 +2,11 @@ angular.module('Controllers', [])
 .controller('HomeController', function($scope, $http, $location, $anchorScroll, anchorSmoothScroll) {
     $scope.title = "Welcome To Angular Todo!";
     $http.get("https://www.googleapis.com/books/v1/volumes?q=HTML5").then(function(response){
-        console.log(response.data.items);
         $scope.books = response.data.items;
         setTimeout(function(){ 
             if(previousSelection !== null){
-                console.log(selection);
                 for(i = 0; i < selection.length; i++){
                     var name = selection[i];
-                    console.log(name);
                     var el = document.getElementById(name);
                     el.classList.add("is-selected");
                 }
@@ -30,28 +27,20 @@ angular.module('Controllers', [])
         }
     }
     $scope.selection = function(index){
-        console.log(selection);
         var elName = "book-" + index;
         var arIndex = selection.indexOf(elName);
-        console.log("index of selected book: " + arIndex);
         if(arIndex == -1){
             selection.push(elName);
-            console.log("added id" + elName + " at index " + arIndex);
         } else {
             selection.splice(arIndex,1);
-            console.log("removed id" + elName + " at index " + arIndex);
 
         }
         sessionStorage.setItem("selection", JSON.stringify(selection))
-        console.log(selection);
         var element = document.getElementById(elName);
-        console.log(element);
         if(element.classList.contains("is-selected")){
             element.classList.remove("is-selected");
             
         } else {
-            element.classList.add("is-selected");
-            console.log("adding class");
         }
     }
 });

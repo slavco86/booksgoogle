0d6272025f3ffced737c0ea464c9680ce1ec7575
SlavCO <slavick.slavick@gmail.com>
2017-09-18 16:47:35 +0100

    added class selection, de-selection, added session storage for saving selected state on page reload

--- js/controller.js
+++ js/controller.js
@@ -2,13 +2,41 @@ angular.module('Controllers', [])
 .controller('HomeController', function($scope, $http, $location, $anchorScroll, anchorSmoothScroll) {
     $scope.title = "Welcome To Angular Todo!";
     $http.get("https://www.googleapis.com/books/v1/volumes?q=HTML5").then(function(response){
-        console.log(response.data.items);
         $scope.books = response.data.items;
+        setTimeout(function(){ 
+            if(previousSelection !== null){
+                for(i = 0; i < selection.length; i++){
+                    var name = selection[i];
+                    var el = document.getElementById(name);
+                    el.classList.add("is-selected");
+                }
+            }
+        }, 250);
     });
     $scope.scroll = function(x){
         $anchorScroll.yOffset = 350;
         var location = "book-" + x;
         $anchorScroll(location);
-        console.log(location);
     };
+    var selection = []
+    var previousSelection = JSON.parse(sessionStorage.getItem("selection"));
+    for(q = 0; q < previousSelection.length; q++){
+        selection.push(previousSelection[q]);
+    }
+    $scope.selection = function(index){
+        var elName = "book-" + index;
+        var arIndex = selection.indexOf(elName);
+        if(arIndex == -1){
+            selection.push(elName);
+        } else {
+            selection.splice(arIndex,1);
+        }
+        sessionStorage.setItem("selection", JSON.stringify(selection))
+        var element = document.getElementById(elName);
+        if(element.classList.contains("is-selected")){
+            element.classList.remove("is-selected");
+        } else {
+            element.classList.add("is-selected");
+        }
+    }
 });
